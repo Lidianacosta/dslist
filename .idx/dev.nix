734f6be78d5d249faceebc4b0b55b652e6a92eb0
Lidiana Costa <lidianacostasouza@gmail.com>
2024-11-11 20:43:34 +0000

    :tada: initial commit

---
+++ .idx/dev.nix
@@ -0,0 +1,36 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.zulu17
+    pkgs.maven
+  ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "vscjava.vscode-java-pack"
+      "rangav.vscode-thunder-client"
+      "redhat.java"
+      "vscjava.vscode-gradle"
+      "vscjava.vscode-java-debug"
+      "vscjava.vscode-java-dependency"
+      "vscjava.vscode-java-test"
+      "vscjava.vscode-maven"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install = "mvn clean install";
+      };
+      # Runs when a workspace is (re)started
+      onStart = {
+        run-server = "PORT=3000 mvn spring-boot:run";
+      };
+    };
+  };
+}
