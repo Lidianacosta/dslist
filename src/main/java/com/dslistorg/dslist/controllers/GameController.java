851e838fa3c8045ad2f4420abc60f1694945b1b8
Lidiana Costa <lidianacostasouza@gmail.com>
2024-11-12 21:44:35 +0000

    :sparkles: feat: Adding find by id in GameController.

--- src/main/java/com/dslistorg/dslist/controllers/GameController.java
+++ src/main/java/com/dslistorg/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dslistorg.dslist.dto.GameDTO;
 import com.dslistorg.dslist.dto.GameMinDTO;
 import com.dslistorg.dslist.services.GameService;
 
@@ -16,6 +18,11 @@ public class GameController {
 
     @Autowired
     private GameService GameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findAll(@PathVariable Long id) {
+        return GameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
